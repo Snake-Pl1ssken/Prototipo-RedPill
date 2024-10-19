27143672d8352a8e7414b8ba7423533271ce41c3
Snake-Pl1ssken <jorgeluisgarciaorbegoso@gmail.com>
2024-10-19 19:59:53 +0200

    añadida la funcionalidad de pausar el juego con el boton escape faltaria añadirle la funcionalidad a los botones del menu de pausa y embellecer el menu

--- Assets/MenuPausa.cs
+++ Assets/MenuPausa.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MenuPausa : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Input Actions Pause")]
+    [SerializeField] InputActionReference pause;
+    [SerializeField] GameObject pauseScreen;
+
+
+    bool pauseState = false;
+
+
+    void OnEnable()
     {
-        
+        pause.action.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (pause.action.WasPressedThisFrame() && !pauseState)
+        {
+            Debug.Log("saltando");
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0f;
+            pauseState = true;
+        }
+        else if(pause.action.WasPerformedThisFrame() && pauseState)
+        {
+            pauseScreen.SetActive(false);
+            pauseState = false;
+            Time.timeScale = 1f;
+        }
+    }
+
+    void OnDisable()
+    {
+        pause.action.Disable();
     }
 }
