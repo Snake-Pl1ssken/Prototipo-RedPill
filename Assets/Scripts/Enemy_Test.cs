43eae1f9c250ac9d12c7743f625e4b0b937cd211
Snake-Pl1ssken <jorgeluisgarciaorbegoso@gmail.com>
2024-10-30 13:54:39 +0100

    añadido coche de patrulla y fixeado el detector del player

--- Assets/Scripts/Enemy_Test.cs
+++ Assets/Scripts/Enemy_Test.cs
@@ -6,35 +6,37 @@ using static UnityEngine.UI.Image;
 
 public class Enemy_Test : MonoBehaviour
 {
-    [SerializeField] Transform EnemyGunPosition;
-    [SerializeField] float maxDistance;
-    private bool hasLineOfSight = false;
+    //[SerializeField] Transform EnemyGunPosition;
+    //[SerializeField] float maxDistance;
+    //private bool hasLineOfSight = false;
     
     //private float GunPosition;
 
-    private void Start()
-    {
-        EnemyGunPosition = gameObject.transform;
-    }
+    //private void Start()
+    //{
+    //    EnemyGunPosition = gameObject.transform;
+    //}
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Chocando con player");
+            //Debug.Log("Chocando con player");
             MenuPausa.instance.PerderVida();
         }
     }
 
-    private void FixedUpdate()
-    {
-        RaycastHit hitRay;
+    //private void FixedUpdate()
+    //{
+    //    RaycastHit hitRay;
             
-        if(Physics.Raycast(EnemyGunPosition.position, EnemyGunPosition.forward, out hitRay, maxDistance))
-        {
-            Debug.Log("hit" + hitRay.transform.gameObject.name);
-        }
-        Debug.Log(EnemyGunPosition.position);
-    }
+    //    if(Physics.Raycast(EnemyGunPosition.position, EnemyGunPosition.forward, out hitRay, maxDistance))
+    //    {
+    //        Debug.Log("hit" + hitRay.transform.gameObject.name);
+    //        Debug.DrawRay(EnemyGunPosition.position, EnemyGunPosition.forward * 10, Color.green);
+    
+    //    }
+    //    //Debug.Log(EnemyGunPosition.position);
+    //}
 
 }
---
+++ Assets/Scripts/Sight_Test.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+using static UnityEngine.UI.Image;
+
+public class Sight_Test : MonoBehaviour
+{
+    [SerializeField] Transform EnemyGunPosition;
+    [SerializeField] float maxDistance;
+    private bool hasLineOfSight = false;
+
+    //private float GunPosition;
+
+    private void Start()
+    {
+        EnemyGunPosition = gameObject.transform;
+    }
+
+
+    private void FixedUpdate()
+    {
+        RaycastHit hitRay;
+
+        if (Physics.Raycast(EnemyGunPosition.position, EnemyGunPosition.forward, out hitRay, maxDistance))
+        {
+            Debug.Log("hit" + hitRay.transform.gameObject.name);
+            Debug.DrawRay(EnemyGunPosition.position, EnemyGunPosition.forward * 10, Color.green);
+
+        }
+        //Debug.Log(EnemyGunPosition.position);
+    }
+
+}
