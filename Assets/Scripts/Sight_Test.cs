37fc7bb826dd10958f7d6defe86101e7a53fc3d0
Snake-Pl1ssken <jorgeluisgarciaorbegoso@gmail.com>
2024-11-27 13:46:44 +0100

    Añadida escena de menu de inicio a modo de test, aun falta la funcionalida de el menu y sus botones

--- Assets/Scripts/Sight_Test.cs
+++ Assets/Scripts/Sight_Test.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class Sight_Test : MonoBehaviour
 {
     [SerializeField] Transform EnemyGunPosition;
+    [SerializeField] GameObject mePatrolEnemy;
     [SerializeField] float maxDistance;
     [SerializeField] float speed;
+    bool iSeetheEnemy = false;
+    float timePassed;
 
     private void Start()
     {
@@ -22,6 +25,19 @@ public class Sight_Test : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (iSeetheEnemy)
+        {
+            timePassed += Time.deltaTime;
+            if (timePassed >= 3f)
+            { 
+                Debug.Log("Me destruyo");
+                Destroy(mePatrolEnemy);
+            }
+        }
+    }
+
     private void FixedUpdate()
     {
         if (Physics.Raycast(EnemyGunPosition.position, EnemyGunPosition.forward, out RaycastHit hitRay, maxDistance))
@@ -30,6 +46,8 @@ public class Sight_Test : MonoBehaviour
             Debug.DrawRay(EnemyGunPosition.position, EnemyGunPosition.forward * 10, Color.green); //Solo Scene
             Rigidbody rb = GetComponent<Rigidbody>();
             rb.velocity = transform.forward * speed;
+            iSeetheEnemy = true;
+     //       Debug.Log("iSeetheEnemy a TRUE");
         }
     }
 
